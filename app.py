0a573ed6e5d3749b557483779e73944a2b33372f
gabrielsouza110 <ozoriogabriel737@gmail.com>
2025-08-17 14:53:12 -0300

    i imported our dataset

--- app.py
+++ app.py
@@ -2,30 +2,16 @@ import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data = {
-'name' : ['Gabriel','João', 'Pedro'],
-'age': [17,18,20],
-'City': ['Januária', 'São Francisco','Maria da Cruz']
-}
+data_df = pd.read_csv("autism_screening.csv")
+st.set_page_config(layout="wide")
 
-df = pd.DataFrame(data)
+st.title('Análise de Dados com Streamlit')
 
-st.title('Hello World')
-st.write('This is a initial .py file using Streamlit!')
+st.write(data_df.head(15))
+st.header("Estatísticas Descritivas")
+st.write(data_df.describe())
 
-st.header('This is a header')
-st.subheader('This is a subheader')
 
-st.text('This is a text')#no fomratting
-st.markdown('This is a **markdown** text')#with formatting
 
-st.dataframe(df) # Display the dataframe
-st.table(df) # Display the dataframe as a static table
-
-fig, ax = plt.subplots()
-ax.bar(df['name'], df['age'])
-ax.set_xlabel('name')   # Define o rótulo do eixo x
-ax.set_ylabel('age')    # Define o rótulo do eixo y
-st.pyplot(fig)  # Display the matplotlib figure
 
 
